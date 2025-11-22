b782b22bc316cc02a23aef158bc4557739a3d4b4
justappgrabbin <bookingalexismcmillion@gmail.com>
2025-11-22 14:14:54 -0800

    Create App.tsx

---
+++ Src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      <h1>TSX App on Netlify</h1>
+      <p>Your file is running!</p>
+    </div>
+  );
+}
