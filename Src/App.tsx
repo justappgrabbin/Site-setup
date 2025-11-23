e8af18f89a2b216b4fe51e75e7aa0de85c9dfbbd
justappgrabbin <bookingalexismcmillion@gmail.com>
2025-11-22 18:05:57 -0800

    Delete Src directory

--- Src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold mb-4">Paste Your CustomLLMChat Component</h1>
-      <p className="opacity-80">Open <code>src/CustomLLMChat.tsx</code> and paste your full cthereomponent .</p>
-    </div>
-  );
-}
